28bc8df23a6d969a9dcafd2e25638a308bffc1ac
gcassulo <gcassulo@mardelplata.gob.ar>
2024-06-04 12:07:55 -0300

    merge

---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/AntenasConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class AntenasConfiguration :IEntityTypeConfiguration<Antenas>
+    {
+        public void Configure(EntityTypeBuilder<Antenas> builder)
+        {
+            builder
+                .HasKey(k => k.IdAntena);
+
+            builder
+                .Property(p => p.IdTipoAntena)
+                .ValueGeneratedOnAdd()
+                .IsRequired(true);
+
+            builder
+                .Property(p => p.IdPrestador)
+                .IsRequired(true);
+
+            builder
+                .Property(p => p.IdEstado)
+                .IsRequired(true);
+
+            builder.Property(p => p.IdExpediente)
+                .IsRequired(true);
+
+            builder
+                .Property(p => p.AlturaSoporte)
+                .IsRequired(true);
+            builder
+                .Property(p => p.AlturaTotal)
+                .IsRequired(true);
+            builder
+                .Property(p => p.CellId)
+               
+                .IsRequired(true);
+            builder
+                .Property(p => p.CodigoSitio)
+                .IsRequired(true);
+            builder
+                .Property(p => p.Declarada)
+                .IsRequired(true);
+            builder
+                .Property(p => p.Direccion)
+                .IsRequired(true);
+            builder
+                .Property(p => p.Emplazamiento)
+                .IsRequired(true);
+            builder
+                .Property(p => p.Observaciones)
+                .IsRequired(true);
+            builder
+                .Property(p => p.SalaEquipos)
+                .IsRequired(true);
+            builder
+                .Property(p => p.TipoMimetizado)
+                .IsRequired(true);
+        }
+    }
+}
---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/ApoderadosConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class ApoderadosConfiguration:IEntityTypeConfiguration<Apoderados>
+    {
+        public void Configure(EntityTypeBuilder<Apoderados> builder) 
+        {
+            builder
+                .HasKey(k => k.IdApoderado);
+            builder
+                .Property(p => p.Nombre)
+                .IsRequired(true);
+            builder
+                .Property(p => p.Apelllido)
+                .IsRequired(true);
+            builder
+                .Property(p => p.NroDoc)
+                .IsRequired(true);
+        }
+    }
+}
