1acb859a2ef87a117ab87d37b621f7be709d04a4
sebastianperez <sebastianperez@mardelplata.gob.ar>
2024-08-20 14:27:35 -0300

    tabla

---
+++ CodigoFuente/antenas/API/DataSchema/MEC_CarRevista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace API.DataSchema
+{
+    public class MEC_CarRevista
+    {
+        public int IdCarRevista {  get; set; }
+        public string CodPcia { get; set; }
+        public string Descripcion {  get; set; }
+        public string CodMgp { get; set; }
+
+    }
+}
