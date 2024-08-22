8ea6a0904769fef032e32eefe1cf9a04d2264e9f
gcassulo <gcassulo@mardelplata.gob.ar>
2024-08-22 12:59:53 -0300

    agrega plantilla react

---
+++ CodigoFuente/antenas/React/src/App.js
@@ -0,0 +1,190 @@
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Sidenav from "examples/Sidenav";
+import theme from "assets/theme";
+import themeDark from "assets/theme-dark";
+import routes from "routes";
+import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
+import jwt_decode from "jwt-decode";
+import SignInBasic from "../src/layouts/authentication/sign-in/basic/index";
+import AltaConservadora from "layouts/pages/Pruebas/Conservadora/AltaConservadora";
+import ConservadoraVer from "layouts/pages/Pruebas/Conservadora/ConservadoraVer";
+import ConservadoraListado from "layouts/pages/Pruebas/Conservadora/index";
+
+export default function App() {
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const { pathname } = useLocation();
+
+  // Validate token
+  const isTokenExpired = (token, subtractSeconds = 0) => {
+    const decodedToken = jwt_decode(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+    const expirationTime = decodedToken.exp - subtractSeconds;
+    return expirationTime < currentTime;
+  };
+
+  const isTokenAvailable = () => {
+    const token = sessionStorage.getItem("token");
+    if (!token || isTokenExpired(token)) {
+      sessionStorage.removeItem("token");
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    const storageEventListener = (event) => {
+      if (event.storageArea === sessionStorage && event.key === "token") {
+        if (!isTokenAvailable()) {
+          window.location.href = "/authentication/sign-in/basic";
+        }
+      }
+    };
+
+    window.addEventListener("storage", storageEventListener);
+    return () => {
+      window.removeEventListener("storage", storageEventListener);
+    };
+  }, []);
+
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={<route.component />} key={route.key} />;
+      }
+      return null;
+    });
+
+  //Filtrado de las rutas + roles
+
+  const filterRoutesByRole = (routes, role) => {
+    return routes
+      .filter((route) => !route.roles || route.roles.includes(role))
+      .map((route) => {
+        if (route.collapse) {
+          return {
+            ...route,
+            collapse: filterRoutesByRole(route.collapse, role),
+          };
+        }
+        return route;
+      });
+  };
+
+  const getRoleFromToken = () => {
+    const token = sessionStorage.getItem("token");
+    if (!token) return null;
+    try {
+      const decodedToken = jwt_decode(token);
+      return decodedToken.role;
+    } catch (error) {
+      console.error("Failed to decode token", error);
+      return null;
+    }
+  };
+
+  const role = getRoleFromToken();
+  const filteredRoutes = filterRoutesByRole(routes, role);
+
+  //Agregar Roles para proteccion por URL !!!!
+  //Coincidir roles + path(route) con routes.js para no generar conflictos
+
+  const RoutesProtegidas = [
+    {
+      path: "/ConservadoraFE/Nuevo",
+      component: AltaConservadora,
+      roles: ["admin"],
+    },
+    {
+      path: "/ConservadoraFE",
+      component: ConservadoraListado,
+      roles: ["admin", "user"],
+    },
+    {
+      path: "/ConservadoraFE/:id",
+      component: ConservadoraVer,
+      roles: ["admin", "user"],
+    },
+  ];
+
+  const login = [
+    {
+      path: "/authentication/sign-in/basic",
+      component: SignInBasic,
+    },
+  ];
+
+  const filteredProtectedRoutes = RoutesProtegidas.filter(
+    (route) => !route.roles || route.roles.includes(role)
+  );
+
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {layout === "dashboard" && (
+        <Sidenav
+          color={sidenavColor}
+          brandName="Elevadores"
+          brand="https://www.mardelplata.gob.ar/sites/all/themes/mgp/ico/favicon.ico"
+          routes={filteredRoutes}
+          onMouseEnter={handleOnMouseEnter}
+          onMouseLeave={handleOnMouseLeave}
+        />
+      )}
+      <Routes>
+        {login.map((route) => (
+          <Route key={route.path} path={route.path} element={<route.component />} />
+        ))}
+        <Route path="*" element={<Navigate to="/authentication/sign-in/basic" />} />
+        {filteredProtectedRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={
+              isTokenAvailable() ? (
+                <route.component />
+              ) : (
+                <Navigate to="/authentication/sign-in/basic" />
+              )
+            }
+          />
+        ))}
+      </Routes>
+    </ThemeProvider>
+  );
+}
---
+++ CodigoFuente/antenas/React/src/components/SelectField/index.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import FormField from "layouts/pages/account/components/FormField";
+
+function SelectField({
+  label,
+  name,
+  apiUrl,
+  valueField,
+  optionField,
+  formData,
+  handleChange,
+  customOptions,
+}) {
+  const [options, setOptions] = useState([]);
+  const token = sessionStorage.getItem("token");
+
+  useEffect(() => {
+    // Verificar si apiUrl está definido y no es nulo
+    if (apiUrl && apiUrl.includes("${idConservadora}")) {
+      const idConservadora = formData["idConservadora"];
+      const url = apiUrl.replace("${idConservadora}", idConservadora);
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${token}`, // Envía el token en los headers
+          },
+        })
+        .then((response) => {
+          setOptions(response.data);
+        })
+        .catch((error) => {
+          console.error("Error al cargar las opciones:", error);
+        });
+    } else if (apiUrl) {
+      axios
+        .get(apiUrl, {
+          headers: {
+            Authorization: `Bearer ${token}`, // Envía el token en los headers
+          },
+        })
+        .then((response) => {
+          setOptions(response.data);
+        })
+        .catch((error) => {
+          console.error("Error al cargar las opciones:", error);
+        });
+    }
+  }, [apiUrl, formData.idConservadora, token]);
+
+  const combinedOptions = [...customOptions, ...options];
+
+  return (
+    <div>
+      <Autocomplete
+        options={combinedOptions}
+        getOptionLabel={(option) =>
+          optionField === "nombre" && option.apellido
+            ? `${option.nombre} ${option.apellido}`
+            : option[optionField]
+        }
+        value={combinedOptions.find((option) => option[valueField] === formData[name]) || null}
+        onChange={(event, newValue) => {
+          handleChange({ target: { name, value: newValue ? newValue[valueField] : null } });
+        }}
+        renderInput={(params) => <FormField {...params} label={label} />}
+      />
+    </div>
+  );
+}
+
+SelectField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  apiUrl: PropTypes.string,
+  valueField: PropTypes.string.isRequired,
+  optionField: PropTypes.string.isRequired,
+  formData: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  customOptions: PropTypes.array,
+};
+
+SelectField.defaultProps = {
+  apiUrl: null,
+  customOptions: [],
+};
+
+export default SelectField;
