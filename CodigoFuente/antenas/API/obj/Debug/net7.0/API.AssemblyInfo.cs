5a3c89104b180399c24bc1d102f6be75043793bd
gabrielalopez <gabrielalopez@mardelplata.gob.ar>
2024-08-26 15:25:25 -0300

    GLO requerimientos funcionales

--- CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629be3179741f9fc1fd7f5440c1b0c55f7b8d17c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c919cccf7846f0d4298dfe87389298980ebe2a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
