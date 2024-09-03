4ce365282748ee5dfebf54edd232a7dc74ab2cb1
vuriarte <vuriarte@mardelplata.gov.ar>
2024-09-03 09:34:16 -0300

    cabecera

---
+++ CodigoFuente/antenas/API/DataSchema/MEC_CabeceraLiquidacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace API.DataSchema
+{
+    public class MEC_CabeceraLiquidacion
+    {
+    
+        public int IdCabecera { get; set; }
+        public int idTipoLiquidacion { get; set; }
+        public string MesLiquidacion { get; set; }
+        public string AnioLiquidacion { get; set; }
+        public string Usuario { get; set; }
+        public string Observaciones { get; set; }
+        public DateTime InicioLiquidacion { get; set; }
+        public DateTime FinLiquidacion { get; set; }
+        public string Estado { get; set; }
+        public string Vigente { get; set; }
+        public virtual MEC_TiposLiquidaciones? TipoLiquidacion { get; set; }
+     }
+}
