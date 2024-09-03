8745960007cf1dc29a29a27efd087a29e74b31c3
vuriarte <vuriarte@mardelplata.gov.ar>
2024-09-03 09:34:44 -0300

    cabecera

---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_CabeceraLiquidacionConfiguration.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_CabeceraLiquidacionConfiguration : IEntityTypeConfiguration<MEC_CabeceraLiquidacion>
+    {
+        public void Configure(EntityTypeBuilder<MEC_CabeceraLiquidacion> builder)
+        {
+            builder
+                .HasKey(e => e.IdCabecera);
+
+            builder
+                .Property(e => e.IdCabecera)
+                .ValueGeneratedOnAdd();
+
+            builder
+                .HasOne(e => e.TipoLiquidacion)
+                .WithOne()
+                .HasForeignKey<MEC_TiposLiquidaciones>(k => k.IdTipoLiquidacion)
+                .IsRequired(true);
+
+            builder
+                .Navigation(e => e.TipoLiquidacion)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder.Property(e => e.MesLiquidacion)
+                .HasColumnType("char(2)")
+                .IsFixedLength(true)
+                .IsRequired(true); 
+
+            builder.Property(e => e.AnioLiquidacion)
+                .HasColumnType("char(2)")
+                .IsFixedLength(true)
+                .IsRequired(false); 
+
+            builder.Property(e => e.Usuario)
+                .HasColumnType("varchar(50)")
+                .IsFixedLength(true)
+                .IsRequired(true);
+
+            builder.Property(e => e.Observaciones)
+                .HasColumnType("varchar(1000)")
+                .IsFixedLength(true)
+                .IsRequired(true);
+            
+            builder.Property(e => e.InicioLiquidacion)
+                .HasColumnType("DateTime")
+                .IsFixedLength(true)
+                .IsRequired(true);
+            
+            builder.Property(e => e.FinLiquidacion)
+                .HasColumnType("DateTime")
+                .IsFixedLength(true)
+                .IsRequired(true);
+
+            builder.Property(e => e.Estado)
+                .HasColumnType("char(1)")
+                .IsFixedLength(true)
+                .IsRequired(true);
+
+          
+
+            builder.Property(e => e.Vigente)
+                .HasColumnType("char(1)")
+                .IsFixedLength(true)
+                .IsRequired(true); 
+        }
+    }
+}
---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_TiposLiquidacionesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_TiposLiquidacionesConfiguration : IEntityTypeConfiguration<MEC_TiposLiquidaciones>
+    {
+        public void Configure(EntityTypeBuilder<MEC_TiposLiquidaciones> builder)
+        {
+            builder
+                .HasKey(k => k.IdTipoLiquidacion);
+
+            builder
+                .Property(p => p.IdTipoLiquidacion)
+                .ValueGeneratedOnAdd();
+          
+            builder.Property(e => e.Descripcion)
+                .HasColumnType("varchar(100)")
+                .IsFixedLength(false)
+                .IsRequired(true); 
+
+            builder.Property(e => e.Vigente)
+                .HasColumnType("char(1)")
+                .IsFixedLength(true)
+                .IsRequired(true); 
+
+        }
+    }
+}
