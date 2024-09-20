8146ae726da52cd65fdd1066687f655468aca571
gabrielalopez <gabrielalopez@mardelplata.gob.ar>
2024-09-20 11:41:09 -0300

    glo

---
+++ CodigoFuente/antenas/API/Controllers/ImportarMecanizadasController.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using API.DataSchema;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Controllers
+{
+    [ApiController]  //[Authorize(Roles = "Admin")]
+    [AllowAnonymous]
+    [Route("[controller]")]
+    public class ImportarMecanizadasController : ControllerBase
+    {
+        private readonly IImportacionMecanizadaService<MEC_TMPMecanizadas> _importacionMecanizadaService;
+
+        public ImportarMecanizadasController(IImportacionMecanizadaService<MEC_TMPMecanizadas> importacionService)
+        {
+            _importacionMecanizadaService = importacionService;
+        }
+
+        [HttpPost("ImportarExcel")]
+        public async Task<IActionResult> ImportarExcel(IFormFile file)
+        {
+            try
+            {
+                await _importacionMecanizadaService.ImportarExcel(file);
+                return Ok("Importación exitosa.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
--- CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b3c3018988c69d01d9549694607a9f6843aad7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a815eddbae946e5ca10d7acb16e27f4426fa73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
