99555591bec741f46b594219f0932201088e49d4
gabrielalopez <gabrielalopez@mardelplata.gob.ar>
2024-10-24 10:41:32 -0300

    documentacion

---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_TMPErroresCarRevistaConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_TMPErroresCarRevistaConfiguration : IEntityTypeConfiguration<MEC_TMPErroresCarRevista>
+    {
+        public void Configure(EntityTypeBuilder<MEC_TMPErroresCarRevista> builder)
+        {
+            builder
+                .HasKey(k => k.IdTMPErrorCarRevista);
+
+            builder
+                .Property(p => p.IdTMPErrorCarRevista)
+                .ValueGeneratedOnAdd();
+
+            builder
+                .HasOne(e => e.Cabecera)
+                .WithOne()
+                .HasForeignKey<MEC_CabeceraLiquidacion>(e => e.IdCabecera)
+                .IsRequired(true);
+
+            builder
+                .Navigation(e => e.Cabecera)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder.Property(e => e.CaracterRevista)
+                .HasColumnType("varchar(20)")
+                .IsFixedLength(true)
+                .IsRequired(false); 
+        }
+    }
+}
--- CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403094665182cda431da79f77c2e84f61abcd27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2761ec51f6882efb66faadaca13663cb6bb87bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
