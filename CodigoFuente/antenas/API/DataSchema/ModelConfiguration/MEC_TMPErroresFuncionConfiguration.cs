c2b2b3e69b302576a6c27030f10d58408e16b331
gcassulo <gcassulo@mardelplata.gob.ar>
2024-10-24 09:51:58 -0300

    tablas tmp

---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_TMPErroresFuncionConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_TMPErroresFuncionConfiguration : IEntityTypeConfiguration<MEC_TMPErroresFuncion>
+    {
+        public void Configure(EntityTypeBuilder<MEC_TMPErroresFuncion> builder)
+        {
+            builder
+                .HasKey(k => k.IdTMPErrorFuncion);
+
+            builder
+                .Property(p => p.IdTMPErrorFuncion)
+                .ValueGeneratedOnAdd();
+
+            builder
+                .HasOne(e => e.Cabecera)
+                .WithOne()
+                .HasForeignKey<MEC_CabeceraLiquidacion>(e => e.IdCabecera)
+                .IsRequired(true);
+
+            builder
+                .Navigation(e => e.Cabecera)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder.Property(e => e.CodFuncion)
+                .HasColumnType("varchar(20)")
+                .IsFixedLength(true)
+                .IsRequired(false); 
+        }
+    }
+}
---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_TMPErroresTipoEstablecimientoConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_TMPErroresTipoEstablecimientoConfiguration : IEntityTypeConfiguration<MEC_TMPErroresTiposEstablecimientos>
+    {
+        public void Configure(EntityTypeBuilder<MEC_TMPErroresTiposEstablecimientos> builder)
+        {
+            builder
+                .HasKey(k => k.IdTMPErrorTipoEstablecimiento);
+
+            builder
+                .Property(p => p.IdTMPErrorTipoEstablecimiento)
+                .ValueGeneratedOnAdd();
+
+            builder
+                .HasOne(e => e.Cabecera)
+                .WithOne()
+                .HasForeignKey<MEC_CabeceraLiquidacion>(e => e.IdCabecera)
+                .IsRequired(true);
+
+            builder
+                .Navigation(e => e.Cabecera)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder.Property(e => e.TipoOrganizacion)
+                .HasColumnType("varchar(20)")
+                .IsFixedLength(true)
+                .IsRequired(false); 
+        }
+    }
+}
