69a9425bb06993585744954916a99b917bf98a1b
gabrielalopez <gabrielalopez@mardelplata.gob.ar>
2024-10-25 09:45:18 -0300

    documentacion

--- CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403094665182cda431da79f77c2e84f61abcd27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17c762022579a20fb004e8e0a12945672c397dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
