ba55aa654e853a711f81e5fef2b13756cd3ab23e
gabrielalopez <gabrielalopez@mardelplata.gob.ar>
2024-10-25 09:45:18 -0300

    documentacion

--- CodigoFuente/antenas/CoreServices/obj/Debug/net7.0/CoreServices.Test.AssemblyInfo.cs
+++ CodigoFuente/antenas/CoreServices/obj/Debug/net7.0/CoreServices.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreServices.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403094665182cda431da79f77c2e84f61abcd27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17c762022579a20fb004e8e0a12945672c397dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreServices.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreServices.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
