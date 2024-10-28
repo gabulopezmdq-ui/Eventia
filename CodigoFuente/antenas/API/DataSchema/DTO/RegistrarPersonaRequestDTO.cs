ddb3ae35767ea74505cf664b3301973a80abd9c9
gcassulo <gcassulo@mardelplata.gob.ar>
2024-10-28 11:29:59 -0300

    push

---
+++ CodigoFuente/antenas/API/DataSchema/DTO/RegistrarPersonaRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace API.DataSchema.DTO
+{
+    public class RegistrarPersonaRequestDTO
+    {
+        public string DNI { get; set; }
+        public string Legajo { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
