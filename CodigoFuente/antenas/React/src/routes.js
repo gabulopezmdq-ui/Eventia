355306c28b6c86295de51619083a126720f4da43
efrontera <ernestorfrontera@gmail.com>
2024-10-28 10:58:28 -0300

    pof

--- CodigoFuente/antenas/React/src/routes.js
+++ CodigoFuente/antenas/React/src/routes.js
@@ -1,51 +1,4 @@
-/**
-=========================================================
-* Material Dashboard 2 PRO React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-/** 
-  All of the routes for the Material Dashboard 2 PRO React are added here,
-  You can add a new route, customize the routes and delete the routes here.
-
-  Once you add a new route on this file it will be visible automatically on
-  the Sidenav.
-
-  For adding a new route you can follow the existing routes in the routes array.
-  1. The `type` key with the `collapse` value is used for a route.
-  2. The `type` key with the `title` value is used for a title inside the Sidenav. 
-  3. The `type` key with the `divider` value is used for a divider between Sidenav items.
-  4. The `name` key is used for the name of the route on the Sidenav.
-  5. The `key` key is used for the key of the route (It will help you with the key prop inside a loop).
-  6. The `icon` key is used for the icon of the route on the Sidenav, you have to add a node.
-  7. The `collapse` key is used for making a collapsible item on the Sidenav that contains other routes
-  inside (nested routes), you need to pass the nested routes inside an array as a value for the `collapse` key.
-  8. The `route` key is used to store the route location which is used for the react router.
-  9. The `href` key is used to store the external links location.
-  10. The `title` key is only for the item with the type of `title` and its used for the title text on the Sidenav.
-  10. The `component` key is used to store the component of its route.
-*/
-
-// Material Dashboard 2 PRO React layouts
-import ProfileOverview from "layouts/pages/profile/profile-overview";
-import Settings from "layouts/pages/account/settings";
-import SignInBasic from "layouts/authentication/sign-in/basic";
-// Material Dashboard 2 PRO React components
-import MDAvatar from "components/MDAvatar";
-
-// @mui icons
 import Icon from "@mui/material/Icon";
-
-// Images
 import PersonaListados from "layouts/pages/Pruebas/Persona";
 import ListadoCarRevista from "layouts/pages/Pruebas/CarRevista/index";
 import ListadoTipoEstablecimiento from "layouts/pages/Pruebas/TipoEstablecimiento";
@@ -59,6 +12,7 @@ import Establecimientos from "layouts/pages/Pruebas/Establecimientos";
 import TiposFunciones from "layouts/pages/Pruebas/TiposFunciones";
 import TipoLiquidaciones from "layouts/pages/Pruebas/TipoLiquidaciones";
 import TipoCategorias from "layouts/pages/Pruebas/TipoCategorias";
+import PlantaFuncional from "layouts/pages/Pruebas/PlantaFuncional";
 
 const routes = [
   {
@@ -93,6 +47,12 @@ const routes = [
         key: "listadoPersona",
         route: "/PersonaFE",
         component: <PersonaListados />,
+      },
+      {
+        name: "Planta Funcional",
+        key: "plantaFuncional",
+        route: "/PlantaFuncionalFE",
+        component: <PlantaFuncional />,
       },
     ],
   },
