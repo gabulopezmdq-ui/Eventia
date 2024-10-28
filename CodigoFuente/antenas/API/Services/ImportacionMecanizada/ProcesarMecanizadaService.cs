e16720ab4d10c11bd424854f9bfe7a8abad0a241
gcassulo <gcassulo@mardelplata.gob.ar>
2024-10-28 14:53:44 -0300

    tmp

--- CodigoFuente/antenas/API/Services/ImportacionMecanizada/ProcesarMecanizadaService.cs
+++ CodigoFuente/antenas/API/Services/ImportacionMecanizada/ProcesarMecanizadaService.cs
@@ -14,20 +14,55 @@ namespace API.Services
     {
         private readonly DataContext _context;
 
-        public ProcesarMecanizadaService(DataContext context) // Cambiar el nombre aquí para que coincida con el nombre de la clase
+        public ProcesarMecanizadaService(DataContext context) 
         {
             _context = context;
         }
 
         public async Task PreprocesarAsync(int idCabecera)
         {
-            // Validar NroEstab y agregar errores a TMP_ErroresEstablecimientos
+            // Realizar todas las validaciones
             await ValidarNroEstabAsync(idCabecera);
-
-            // Validar CodFuncion y agregar errores a TMP_ErroresFuncion
             await ValidarCodFuncionAsync(idCabecera);
-
             await ValidarCodLiquidacionAsync(idCabecera);
+            await ValidarCarRevistaAsync(idCabecera);
+            await ValidarTipoOrgAsync(idCabecera);
+
+            // Comprobar si hay errores
+            var tieneErrores = await _context.MEC_TMPErroresEstablecimientos.AnyAsync(e => e.IdCabecera == idCabecera) ||
+                               await _context.MEC_TMPErroresFuncion.AnyAsync(e => e.IdCabecera == idCabecera) ||
+                               await _context.MEC_TMPErroresConceptos.AnyAsync(e => e.IdCabecera == idCabecera) ||
+                               await _context.MEC_TMPErroresCarRevista.AnyAsync(e => e.IdCabecera == idCabecera) ||
+                               await _context.MEC_TMPErroresTiposEstablecimientos.AnyAsync(e => e.IdCabecera == idCabecera);
+
+            if (tieneErrores)
+            {
+                // Borrar los registros de TMPMecanizadas para la cabecera en cuestión
+                var registrosParaEliminar = await _context.MEC_TMPMecanizadas
+                    .Where(m => m.idCabecera == idCabecera)
+                    .ToListAsync();
+
+                _context.MEC_TMPMecanizadas.RemoveRange(registrosParaEliminar);
+                await _context.SaveChangesAsync();
+
+                // Mostrar un mensaje indicando que hay errores
+                // Aquí puedes lanzar una excepción o retornar un objeto de respuesta que contenga los errores
+                throw new Exception("El archivo contiene errores y/o registros no encontrados en las tablas paramétricas. Debe corregir el archivo y volver a importarlo.");
+
+                // Si tienes un modelo para manejar la cabecera de estados:
+                var estadoCabecera = new MEC_CabeceraLiquidacion
+                {
+                    //IdCabecera = idCabecera,
+                    //Fecha = DateTime.UtcNow,
+                    //Usuario = "usuario", // Aquí deberías obtener el usuario actual
+                    //Estado = "P", // Pendiente de Importación
+                    //Observaciones = "Archivo contiene errores – Se deberá corregir y volver a importar"
+                };
+
+                await _context.MEC_CabeceraLiquidacion.AddAsync(estadoCabecera);
+                await _context.SaveChangesAsync();
+            }
+
         }
 
         private async Task ValidarNroEstabAsync(int idCabecera)
@@ -133,6 +168,32 @@ namespace API.Services
                 });
 
                 await _context.MEC_TMPErroresCarRevista.AddRangeAsync(erroresConceptos);
+                await _context.SaveChangesAsync();
+            }
+        }
+        private async Task ValidarTipoOrgAsync(int idCabecera)
+        {
+            // Obtener todos los Tipo Organizacionpara la cabecera en TMPMecanizadas
+            var tipoOrgTMP = await _context.MEC_TMPMecanizadas
+                                              .Where(m => m.idCabecera == idCabecera)
+                                              .Select(m => m.TipoOrganizacion)
+                                              .Distinct()
+                                              .ToListAsync();
+
+            // Verificar la existencia en MEC_CarRevista
+            var tipoOrgInvalidos = tipoOrgTMP.Where(cod =>
+                !_context.MEC_TiposEstablecimientos.Any(f => f.CodTipoEstablecimiento == cod)).ToList();
+
+            // Insertar errores en TMP_ErroresFuncion si hay CodFuncion inválidos
+            if (tipoOrgInvalidos.Any())
+            {
+                var erroresTipoOrg = tipoOrgInvalidos.Select(cod => new MEC_TMPErroresTiposEstablecimientos
+                {
+                    IdCabecera = idCabecera,
+                    TipoOrganizacion = cod
+                });
+
+                await _context.MEC_TMPErroresTiposEstablecimientos.AddRangeAsync(erroresTipoOrg);
                 await _context.SaveChangesAsync();
             }
         }
