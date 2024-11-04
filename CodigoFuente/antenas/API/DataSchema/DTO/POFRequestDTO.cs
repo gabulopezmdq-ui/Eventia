8cb96dd7b772916b91a9f63382c652313cfc33a5
gcassulo <gcassulo@mardelplata.gob.ar>
2024-11-04 15:44:29 -0300

    correcion

--- CodigoFuente/antenas/API/DataSchema/DTO/POFRequestDTO.cs
+++ CodigoFuente/antenas/API/DataSchema/DTO/POFRequestDTO.cs
@@ -5,17 +5,15 @@ using System.IO;
 
 namespace API.DataSchema.DTO
 {
-    public class CabLiquidacionDTO
+    public class POFRequestDTO
     {
-        public int IdCabecera { get; set; }
-        public int IdTipoLiquidacion { get; set; }
-        public string MesLiquidacion { get; set; }
-        public string AnioLiquidacion { get; set; }
-        public int IdUsuario { get; set; }
-        public string Observaciones { get; set; }
-        public DateTime InicioLiquidacion { get; set; }
-        public DateTime FinLiquidacion { get; set; }
-        public string Estado { get; set; }
-        public string Vigente { get; set; }
+        public PersonaRequest Persona { get; set; }
+        public int IdEstablecimiento { get; set; }
+    }
+
+    public class PersonaRequest
+    {
+        public string DNI { get; set; }
+        public string Legajo { get; set; }
     }
 }
