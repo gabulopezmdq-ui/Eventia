e0a95256dd92ef32f7b085ff59df9d0ca9ea46cf
leo <lramirez@mardelplata.gov.ar>
2024-11-05 12:33:59 -0300

    dto

--- CodigoFuente/antenas/API/Services/UsXRol/IUsXRolService.cs
+++ CodigoFuente/antenas/API/Services/UsXRol/IUsXRolService.cs
@@ -1,11 +1,14 @@
-﻿using API.DataSchema.DTO;
+﻿using API.DataSchema;
+using API.DataSchema.DTO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace API.Services.UsXRol
 {
     public interface IUsXRolService
     {
-        Task<bool> UpdateRolesAsync(UPRolXUsuarioDto dto);
+        Task<bool> UpdateRolesAsync(UsuarioRolDto dto);  // Usamos el DTO centralizado aquí
+        Task<IEnumerable<object>> GetAllRolesXUsuariosAsync();
+        Task<MEC_RolesXUsuarios> AddRolToUsuarioAsync(RolXUsuarioDto dto);
     }
 }
-
