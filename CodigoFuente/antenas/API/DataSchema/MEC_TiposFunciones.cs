3c43a409fe529b9b799ba9cfc2dc441cafec031e
gcassulo <gcassulo@mardelplata.gob.ar>
2024-11-13 13:34:33 -0300

    merge parametricas

--- CodigoFuente/antenas/API/DataSchema/MEC_TiposFunciones.cs
+++ CodigoFuente/antenas/API/DataSchema/MEC_TiposFunciones.cs
@@ -7,7 +7,7 @@ namespace API.DataSchema
     {
         public int IdTipoFuncion { get; set; }
         public string CodFuncion { get; set; }
-        public string CodFuncionMGP { get; set; }
+        public string? CodFuncionMGP { get; set; }
         public string Descripcion { get; set; }
         public string Vigente { get; set; }
         public string[] UniqueProperties => new[] { "CodFuncion"}; //  CodCategoria es unico
