60ab9d51e4f80eb12ebbcbe3db5e6b6de79ff1c4
gcassulo <gcassulo@mardelplata.gob.ar>
2024-11-15 10:28:01 -0300

    merge ernesto

--- CodigoFuente/antenas/API/Controllers/POFController.cs
+++ CodigoFuente/antenas/API/Controllers/POFController.cs
@@ -12,8 +12,8 @@ using System.Threading.Tasks;
 namespace API.Controllers
 {
     [ApiController]
-   // [Authorize(Roles = "SuperAdmin, Admin")]
-    [AllowAnonymous]
+   [Authorize(Roles = "SuperAdmin, Admin")]
+   // [AllowAnonymous]
     [Route("[controller]")]
     public class POFController : ControllerBase
     {
