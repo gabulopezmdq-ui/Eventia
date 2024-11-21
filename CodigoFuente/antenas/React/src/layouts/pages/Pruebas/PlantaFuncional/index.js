e39b37a61390489fdab633e357a90ac74ac94fd3
gcassulo <gcassulo@mardelplata.gob.ar>
2024-11-21 14:20:36 -0300

    pof

--- CodigoFuente/antenas/React/src/layouts/pages/Pruebas/PlantaFuncional/index.js
+++ CodigoFuente/antenas/React/src/layouts/pages/Pruebas/PlantaFuncional/index.js
@@ -321,7 +321,6 @@ function PlantaFuncional() {
   return (
     <>
       <DashboardLayout>
-        <p>hola</p>
         <DashboardNavbar />
         <MDBox my={3}>
           <Grid container spacing={2}>
