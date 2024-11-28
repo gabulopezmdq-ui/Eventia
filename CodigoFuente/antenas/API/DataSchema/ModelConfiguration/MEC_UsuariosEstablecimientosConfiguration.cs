58f117e247eab1559c0ebc37f075bed5522ada73
leo <lramirez@mardelplata.gov.ar>
2024-11-28 11:37:48 -0300

    correccion

--- CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_UsuariosEstablecimientosConfiguration.cs
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_UsuariosEstablecimientosConfiguration.cs
@@ -10,6 +10,9 @@ namespace API.DataSchema.ModelConfiguration
                 .HasKey(k => k.IdUsuarioEstablecimiento);
 
             builder
+               .Property(e => e.IdUsuarioEstablecimiento)
+               .ValueGeneratedOnAdd();
+            builder
                 .Property(p => p.IdUsuario)
                 .IsRequired(true);
 
@@ -25,6 +28,39 @@ namespace API.DataSchema.ModelConfiguration
                 .HasColumnType("char(1)")
                 .IsFixedLength(true)
                 .IsRequired(true);
+
+            builder
+                .HasOne(p => p.Usuario)
+                .WithMany(t => t.UsXEstablecimiento)
+                .HasForeignKey(p => p.IdUsuario)
+                .IsRequired(true);
+
+            builder
+                .Navigation(e => e.Usuario)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder
+               .HasOne(p => p.Establecimiento)
+               .WithMany(t => t.UsuarioEstablecimiento)
+               .HasForeignKey(p => p.IdEstablecimiento)
+               .IsRequired(true);
+
+            builder
+                .Navigation(e => e.Establecimiento)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder
+               .HasOne(p => p.Cargo)
+               .WithMany(t => t.UsuarioEstablecimiento)
+               .HasForeignKey(p => p.IdCargo)
+               .IsRequired(true);
+
+            builder
+                .Navigation(e => e.Cargo)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
         }
     }
 }
