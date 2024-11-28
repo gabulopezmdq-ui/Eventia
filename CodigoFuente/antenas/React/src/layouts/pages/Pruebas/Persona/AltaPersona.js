b9fb66b8b00e2561b69123fbbf72a800168e392f
efrontera <ernestorfrontera@gmail.com>
2024-11-28 11:03:03 -0300

    cambios testing Gestion Personas

--- CodigoFuente/antenas/React/src/layouts/pages/Pruebas/Persona/AltaPersona.js
+++ CodigoFuente/antenas/React/src/layouts/pages/Pruebas/Persona/AltaPersona.js
@@ -10,7 +10,7 @@ import { useState } from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
-import Formulario from "components/Formulario";
+import FormularioPersona from "components/FormularioPersona";
 import { Field } from "formik";
 import MDDropzone from "components/MDDropzone";
 
@@ -23,7 +23,9 @@ function AltaPersona() {
   if (id) {
     labelTitulo = "Editar Gestion Persona";
   }
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    vigente: id ? "" : "S", // "vigente" es "S" solo si es alta (id no está presente)
+  });
 
   const handleChange = (e) => {
     setFormData((prevData) => ({
@@ -40,18 +42,23 @@ function AltaPersona() {
         { type: "text", label: "Apellido", name: "apellido", required: true },
         { type: "number", label: "DNI", name: "dni", required: true },
         { type: "text", label: "Legajo", name: "legajo", required: true },
-        {
-          type: "select",
-          label: "Vigente",
-          name: "vigente",
-          customOptions: [
-            { value: "S", label: "Si" },
-            { value: "N", label: "No" },
-          ],
-          valueField: "value",
-          optionField: "label",
-          required: true,
-        },
+        // Solo incluimos el campo "Vigente" si estamos en modo edición (id está presente)
+        ...(id
+          ? [
+              {
+                type: "select",
+                label: "Vigente",
+                name: "vigente",
+                customOptions: [
+                  { value: "S", label: "Si" },
+                  { value: "N", label: "No" },
+                ],
+                valueField: "value",
+                optionField: "label",
+                required: true,
+              },
+            ]
+          : []),
       ],
     },
   ];
@@ -63,7 +70,7 @@ function AltaPersona() {
       <MDBox py={3} mb={20} height="65vh">
         <Grid container justifyContent="center" alignItems="center" sx={{ height: "100%", mt: 8 }}>
           <Grid item xs={12} lg={10}>
-            <Formulario steps={steps} apiUrl={apiUrl} productId={id} />
+            <FormularioPersona steps={steps} apiUrl={apiUrl} productId={id} />
           </Grid>
         </Grid>
       </MDBox>
