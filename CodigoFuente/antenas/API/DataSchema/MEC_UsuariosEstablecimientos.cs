961fd443d80a2c59f8c904c19ca7cc0d1ac797fb
leo <lramirez@mardelplata.gov.ar>
2024-11-28 11:03:35 -0300

    MEC_UsuariosEstablecimientos

---
+++ CodigoFuente/antenas/API/DataSchema/MEC_UsuariosEstablecimientos.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DataSchema
+{
+    public class MEC_UsuariosEstablecimientos
+    {
+        public int IdUsuarioEstablecimiento {  get; set; }
+        public int IdUsuario { get; set; }
+        public int IdEstablecimiento { get; set; }
+        public int IdCargo { get; set; }
+        public string Vigente { get; set; }
+    }
+}
---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_UsuariosEstablecimientosConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_UsuariosEstablecimientosConfiguration : IEntityTypeConfiguration<MEC_UsuariosEstablecimientos>
+    {
+        public void Configure(EntityTypeBuilder<MEC_UsuariosEstablecimientos> builder)
+        {
+            builder
+                .HasKey(k => k.IdUsuarioEstablecimiento);
+
+            builder
+                .Property(p => p.IdUsuario)
+                .IsRequired(true);
+
+            builder
+                .Property(p => p.IdEstablecimiento)
+                .IsRequired(true);
+
+            builder
+                .Property(p => p.IdCargo)
+                .IsRequired(true);
+
+            builder.Property(e => e.Vigente)
+                .HasColumnType("char(1)")
+                .IsFixedLength(true)
+                .IsRequired(true);
+        }
+    }
+}
