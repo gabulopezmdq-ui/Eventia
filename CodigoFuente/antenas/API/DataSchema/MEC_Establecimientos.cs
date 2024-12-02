93656d870c8238f9ba389085764e3bf9ccceaa61
gcassulo <gcassulo@mardelplata.gob.ar>
2024-12-02 11:18:16 -0300

    back

--- CodigoFuente/antenas/API/DataSchema/MEC_Establecimientos.cs
+++ CodigoFuente/antenas/API/DataSchema/MEC_Establecimientos.cs
@@ -23,5 +23,6 @@ namespace API.DataSchema
         public virtual MEC_TiposEstablecimientos? TipoEstablecimientos { get; set; }
         public virtual ICollection<MEC_POF>? POFs { get; set; } = new List<MEC_POF>();
         public virtual ICollection<MEC_UsuariosEstablecimientos>? UsuarioEstablecimiento { get; set; } = new List<MEC_UsuariosEstablecimientos>();
+        public virtual ICollection<MEC_InasistenciasCabecera>? Inasistencias { get; set; } = new List<MEC_InasistenciasCabecera>();
     }
 }
--- CodigoFuente/antenas/API/DataSchema/MEC_InasistenciasCabecera.cs
+++ CodigoFuente/antenas/API/DataSchema/MEC_InasistenciasCabecera.cs
@@ -6,22 +6,14 @@ namespace API.DataSchema
     {
     
         public int IdInasistenciaCabecera { get; set; }
-        public int idTipoLiquidacion { get; set; }
-        public string? LeyendaTipoLiqReporte { get; set; }
-
-        public string MesLiquidacion { get; set; }
-        public string? AnioLiquidacion { get; set; }
-        public int IdUsuario { get; set; }
-        public string? Observaciones { get; set; }
-        public DateTime? InicioLiquidacion { get; set; }
-        public DateTime? FinLiquidacion { get; set; }
-        public string Estado { get; set; }
-        public string? CalculaInasistencias { get; set; }
-        public string? CalculaBajas { get; set; }
-        public int? CantDocentes { get; set; }
-        public decimal? RetenDeno7 { get; set; }
-        public string Vigente { get; set; }
-        public virtual MEC_TiposLiquidaciones? TipoLiquidacion { get; set; }
+        public int IdEstablecimiento { get; set; }
+        public int Confecciono { get; set; } //idUsuario { get; set; }
+        public int Mes { get; set; }
+        public int Anio { get; set; }
+        public DateTime? FechaApertura { get; set; }
+        public DateTime? FechaEntrega { get; set; }
+        public string SinNovedades { get; set; }
+        public virtual MEC_Establecimientos? Establecimientos { get; set; }
         public virtual MEC_Usuarios? Usuarios { get; set; }
     }
 }
