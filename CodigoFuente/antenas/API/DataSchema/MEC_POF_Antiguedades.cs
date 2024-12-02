c45b1cce5c0528521c850161af81566eb714261d
gcassulo <gcassulo@mardelplata.gob.ar>
2024-12-02 10:18:37 -0300

    base de datos

---
+++ CodigoFuente/antenas/API/DataSchema/MEC_POF_Antiguedades.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace API.DataSchema
+{
+    public class MEC_POF_Antiguedades
+    {
+        public int IdPOF { get; set; }
+        public int MesReferencia { get; set; }
+        public int AnioReferencia { get; set; }
+        public int MesAntiguedad { get; set; }
+        public int AnioAntiguedad { get; set; }
+
+        // Propiedades de navegación
+        public virtual MEC_POF? POF { get; set; }
+    }
+}
---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/MEC_POF_AntiguedadesConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DataSchema.ModelConfiguration
+{
+    public class MEC_POF_AntigudadesConfiguration : IEntityTypeConfiguration<MEC_POF_Antiguedades>
+    {
+        public void Configure(EntityTypeBuilder<MEC_POF_Antiguedades> builder)
+        {
+            // Definir clave primaria
+            builder.HasKey(e => e.IdPOF);
+
+            // Definir restricciones para las columnas
+            builder.Property(e => e.MesReferencia)
+                .IsRequired();
+
+            builder.Property(e => e.AnioReferencia)
+                .IsRequired();
+
+            builder.Property(e => e.AnioAntiguedad)
+                .IsRequired(false);
+
+            builder.Property(e => e.MesAntiguedad)
+                .IsRequired(false); 
+            
+            builder
+                .HasOne(p => p.POF)
+                .WithOne()
+                .HasForeignKey<MEC_POF_Antiguedades>(p => p.IdPOF)
+                .IsRequired(true);
+
+            builder
+                .Navigation(e => e.POF)
+                .AutoInclude()
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            // Agregar restricciones CHECK directamente sobre el EntityTypeBuilder
+            builder.HasCheckConstraint("CK_MesReferencia", "[MesReferencia] BETWEEN 1 AND 12");
+            builder.HasCheckConstraint("CK_AnioReferencia", "[AnioReferencia] BETWEEN 2024 AND 3000");
+            builder.HasCheckConstraint("CK_AnioAntiguedad", "[AnioAntiguedad] <= 99");
+            builder.HasCheckConstraint("CK_MesAntiguedad", "[MesAntiguedad] BETWEEN 0 AND 11");
+        }
+    }
+}
