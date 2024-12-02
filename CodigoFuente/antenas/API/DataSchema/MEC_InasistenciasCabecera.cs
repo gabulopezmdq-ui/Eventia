4272cf9b52115351bad7ff3fff054cfd4b7c57cc
gcassulo <gcassulo@mardelplata.gob.ar>
2024-12-02 10:18:37 -0300

    base de datos

---
+++ CodigoFuente/antenas/API/DataSchema/MEC_InasistenciasCabecera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace API.DataSchema
+{
+    public class MEC_CabeceraLiquidacion
+    {
+    
+        public int IdCabecera { get; set; }
+        public int idTipoLiquidacion { get; set; }
+        public string? LeyendaTipoLiqReporte { get; set; }
+
+        public string MesLiquidacion { get; set; }
+        public string? AnioLiquidacion { get; set; }
+        public int IdUsuario { get; set; }
+        public string? Observaciones { get; set; }
+        public DateTime? InicioLiquidacion { get; set; }
+        public DateTime? FinLiquidacion { get; set; }
+        public string Estado { get; set; }
+        public string? CalculaInasistencias { get; set; }
+        public string? CalculaBajas { get; set; }
+        public int? CantDocentes { get; set; }
+        public decimal? RetenDeno7 { get; set; }
+        public string Vigente { get; set; }
+        public virtual MEC_TiposLiquidaciones? TipoLiquidacion { get; set; }
+        public virtual MEC_Usuarios? Usuarios { get; set; }
+    }
+}
