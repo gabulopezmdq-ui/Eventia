4e4fe4f36de8a642af5114c2f990a83865f56149
jcgarcia06 <gjuancruz65@gmail.com>
2024-12-26 11:23:35 -0300

    agrego campos en alta

--- CodigoFuente/antenas/React/src/layouts/pages/Pruebas/CabeceraLiquidacion/AltaCabeceraLiquidacion.js
+++ CodigoFuente/antenas/React/src/layouts/pages/Pruebas/CabeceraLiquidacion/AltaCabeceraLiquidacion.js
@@ -9,13 +9,7 @@ import { useState } from "react";
 // Material Dashboard 2 PRO React examples
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
 import Formulario from "components/Formulario";
-import { Field } from "formik";
-import MDDropzone from "components/MDDropzone";
-
-//Para que el form se pueda utilizar de edicion se tiene que pasar "steps" "apiUrl" "productId" ej: <Formulario steps={steps} apiUrl={apiUrl} productId={id} />
-//Para que sea de crear ej: <Formulario steps={steps} apiUrl={apiUrl} />
 
 function AltaCabeceraLiquidacion() {
   const { id } = useParams();
@@ -41,23 +35,78 @@ function AltaCabeceraLiquidacion() {
           label: "Tipo de Liquidacion",
           name: "idTipoLiquidacion",
           apiUrl: process.env.REACT_APP_API_URL + "TiposLiquidaciones/GetAll",
-          valueField: "idTipoLiquidacion", // Nombre del campo del valor
+          valueField: "idTipoLiquidacion",
           optionField: "descripcion",
+          required: true,
+        },
+        {
+          type: "text",
+          label: "Leyenda Tipo Liq.Reporte",
+          name: "leyendaTipoLiqReporte",
           required: true,
         },
         { type: "text", label: "Mes Liquidacion", name: "mesLiquidacion", required: true },
         { type: "text", label: "Año Liquidacion", name: "anioLiquidacion", required: true },
+        { type: "text", label: "Usuario Liquidacion", name: "usuarioLiquidacion", required: true },
         { type: "text", label: "Observaciones", name: "observaciones", required: true },
         { type: "date", label: "Inicio Liquidacion", name: "inicioLiquidacion", required: true },
         { type: "date", label: "Fin Liquidacion", name: "finLiquidacion", required: true },
-        { type: "text", label: "Estado", name: "estado", required: true },
+        {
+          type: "select",
+          label: "Estado",
+          name: "estado",
+          customOptions: [
+            { value: "P", label: "Pendiente Importación" },
+            { value: "I", label: "Archivo Importado" },
+            { value: "R", label: "Archivo Procesado" },
+            { value: "B", label: "Inasistencias / Bajas Procesado" },
+            { value: "L", label: "En Liquidación" },
+            { value: "C", label: "Liquidación cerrada" },
+          ],
+          valueField: "value",
+          optionField: "label",
+          required: true,
+        },
+        {
+          type: "select",
+          label: "Calcula Inasistencias",
+          name: "calculaInasistencias",
+          customOptions: [
+            { value: "S", label: "Si" },
+            { value: "N", label: "No" },
+          ],
+          valueField: "value",
+          optionField: "label",
+          required: true,
+        },
+        {
+          type: "select",
+          label: "Calcula Bajas",
+          name: "CalculaBajas",
+          customOptions: [
+            { value: "S", label: "Si" },
+            { value: "N", label: "No" },
+          ],
+          valueField: "value",
+          optionField: "label",
+          required: true,
+        },
+        {
+          type: "text",
+          label: "Observaciones Inasistencias",
+          name: "observacionesInasistencias",
+          required: true,
+        },
+        { type: "text", label: "Observaciones Bajas", name: "observacionesBajas", required: true },
+        { type: "number", label: "Cant. Docentes", name: "cantDocentes", required: true },
+        { type: "number", label: "RetenDeno7", name: "retenDeno7", required: true },
         {
           type: "select",
           label: "Vigente",
           name: "vigente",
           customOptions: [
             { value: "S", label: "Si" },
-            { value: "S", label: "No" },
+            { value: "N", label: "No" },
           ],
           valueField: "value",
           optionField: "label",
