fb8bfc43a320459ec7104bb2cbe64d7cefa5bb6f
leo <lramirez@mardelplata.gov.ar>
2025-01-24 11:09:02 -0300

    ConsolidarController

---
+++ CodigoFuente/antenas/API/Controllers/ConsolidarController.cs
@@ -0,0 +1,83 @@
+﻿using API.DataSchema;
+using API.Services;
+using API.Services.ImportacionMecanizada;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+
+namespace API.Controllers
+{
+    [ApiController]
+    //[Authorize(Roles = "SuperAdmin, Admin")]
+    [AllowAnonymous]
+    [Route("[controller]")]
+    public class ConsolidarController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConsolidarMecanizadaService _consolidarMecanizadaService;
+        public ConsolidarController(
+            IConsolidarMecanizadaService consolidarMecanizadaService,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _consolidarMecanizadaService = consolidarMecanizadaService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [HttpPost("ObtenerConteosConsolidado")]
+        public async Task<IActionResult> ObtenerConteosConsolidado(int estadoCabecera)
+        {
+            try
+            {
+                var conteos = await _consolidarMecanizadaService.ObtenerConteosConsolidadoAsync(estadoCabecera);
+                return Ok(conteos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("HabilitarAcciones")]
+        public async Task<IActionResult> HabilitarAcciones(int idEstablecimiento, string estadoCabecera)
+        {
+            try
+            {
+                if (idEstablecimiento <= 0)
+                    return BadRequest("El ID del establecimiento no puede ser menor o igual a cero.");
+
+                if (string.IsNullOrWhiteSpace(estadoCabecera))
+                    return BadRequest("El estado de la cabecera no puede ser nulo o vacío.");
+
+                var habilitado = await _consolidarMecanizadaService.HabilitarAccionesAsync(idEstablecimiento, estadoCabecera);
+
+                // Devolver la respuesta adecuada en formato JSON
+                return Ok(new { Habilitado = habilitado });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = "Ocurrió un error interno en el servidor.", Detalles = ex.Message });
+            }
+        }
+
+
+        [HttpGet("HabilitarCambiarEstadoCabecera")]
+        public async Task<IActionResult> HabilitarCambiarEstadoCabecera(int idCabecera)
+        {
+            try
+            {
+                if (idCabecera <= 0)
+                    return BadRequest("El ID de la cabecera no puede ser menor o igual a cero.");
+
+                var habilitado = await _consolidarMecanizadaService.HabilitarCambiarEstadoCabeceraAsync(idCabecera);
+
+                // Devolver la respuesta con el estado de habilitación
+                return Ok(new { Habilitado = habilitado });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Error = "Ocurrió un error interno en el servidor.", Detalles = ex.Message });
+            }
+        }
+    }
+}
--- CodigoFuente/antenas/API/Controllers/ImportarMecanizadasController.cs
+++ CodigoFuente/antenas/API/Controllers/ImportarMecanizadasController.cs
@@ -24,19 +24,15 @@ namespace API.Controllers
         private readonly IProcesarMecanizadaService<MEC_TMPMecanizadas> _procesarMecanizadaService;
         private readonly ICRUDService<MEC_TMPMecanizadas> _serviceGenerico;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IConsolidarMecanizadaService _consolidarMecanizadaService;
-
         public ImportarMecanizadasController(
             IImportacionMecanizadaService<MEC_TMPMecanizadas> importacionService,
             ICRUDService<MEC_TMPMecanizadas> serviceGenerico,
             IProcesarMecanizadaService<MEC_TMPMecanizadas> procesarMecanizadaService,
-            IConsolidarMecanizadaService consolidarMecanizadaService,
             IHttpContextAccessor httpContextAccessor)
         {
             _importacionMecanizadaService = importacionService;
             _serviceGenerico = serviceGenerico;
             _procesarMecanizadaService = procesarMecanizadaService;
-            _consolidarMecanizadaService = consolidarMecanizadaService;
             _httpContextAccessor = httpContextAccessor;
         }
 
@@ -119,64 +115,5 @@ namespace API.Controllers
                 return StatusCode(500, $"Error al procesar registros: {ex.Message}");
             }
         }
-        
-        /////////////////////////////////////////////////////////
-        
-        [HttpPost("ObtenerConteosConsolidado")]
-        public async Task<IActionResult> ObtenerConteosConsolidado(int estadoCabecera)
-        {
-            try
-            {
-                var conteos = await _consolidarMecanizadaService.ObtenerConteosConsolidadoAsync(estadoCabecera);
-                return Ok(conteos);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [HttpGet("HabilitarAcciones")]
-        public async Task<IActionResult> HabilitarAcciones(int idEstablecimiento, string estadoCabecera)
-        {
-            try
-            {
-                if (idEstablecimiento <= 0)
-                    return BadRequest("El ID del establecimiento no puede ser menor o igual a cero.");
-
-                if (string.IsNullOrWhiteSpace(estadoCabecera))
-                    return BadRequest("El estado de la cabecera no puede ser nulo o vacío.");
-
-                var habilitado = await _consolidarMecanizadaService.HabilitarAccionesAsync(idEstablecimiento, estadoCabecera);
-
-                // Devolver la respuesta adecuada en formato JSON
-                return Ok(new { Habilitado = habilitado });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { Error = "Ocurrió un error interno en el servidor.", Detalles = ex.Message });
-            }
-        }
-
-
-        [HttpGet("HabilitarCambiarEstadoCabecera")]
-        public async Task<IActionResult> HabilitarCambiarEstadoCabecera(int idCabecera)
-        {
-            try
-            {
-                if (idCabecera <= 0)
-                    return BadRequest("El ID de la cabecera no puede ser menor o igual a cero.");
-
-                var habilitado = await _consolidarMecanizadaService.HabilitarCambiarEstadoCabeceraAsync(idCabecera);
-
-                // Devolver la respuesta con el estado de habilitación
-                return Ok(new { Habilitado = habilitado });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { Error = "Ocurrió un error interno en el servidor.", Detalles = ex.Message });
-            }
-        }
-
     }
 }
