a5b9353363228c0b2a08e9ee92b57b7edbbb1c1f
leo <lramirez@mardelplata.gov.ar>
2025-01-24 10:32:16 -0300

    correccion

--- CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ CodigoFuente/antenas/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279f2b39e62d7d0efcef3060b78344f349bbeaea")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
