c73c43c378f54d423f7040896ba5000d038f4254
leo <lramirez@mardelplata.gov.ar>
2025-01-30 10:31:12 -0300

    consolidar

--- CodigoFuente/antenas/API/Services/ImportacionMecanizada/IConsolidarMecanizadaService.cs
+++ CodigoFuente/antenas/API/Services/ImportacionMecanizada/IConsolidarMecanizadaService.cs
@@ -8,10 +8,15 @@ namespace API.Services.ImportacionMecanizada
     public interface IConsolidarMecanizadaService
     {
         Task<object> ObtenerConteosConsolidadoAsync(int idCabecera);
+
         Task<bool> HabilitarAccionesAsync(int idEstablecimiento, string estadoCabecera);
+
         Task<bool> HabilitarCambiarEstadoCabeceraAsync(int idCabecera);
+
         Task<List<MEC_POF>> ObtenerRegistrosPOFNoMecanizadosAsync(int idCabecera, int idEstablecimiento);
+
         Task<bool> ValidarExistenciaAntiguedadAsync(int idPOF);
 
+        Task<bool> ProcesarAltaMecanizadaAsync(AltaMecanizadaDTO datos);
     }
 }
