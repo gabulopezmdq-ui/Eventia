07c98f52374dfdc07bbae85efeae2fc74ad7276e
gcassulo <gcassulo@mardelplata.gob.ar>
2025-03-18 08:55:14 -0300

    correciones back

--- CodigoFuente/antenas/API/Services/ImportacionMecanizada/IConsolidarMecanizadaService.cs
+++ CodigoFuente/antenas/API/Services/ImportacionMecanizada/IConsolidarMecanizadaService.cs
@@ -26,7 +26,7 @@ namespace API.Services.ImportacionMecanizada
         Task<List<MEC_Mecanizadas>> ObtenerSuplentesAsync(int idCabecera, int idEstablecimiento);
         Task<List<MEC_Mecanizadas>> ObtenerMecanizadas(int idCabecera, int idEstablecimiento);
         Task<List<object>> ObtenerPOFsSimplificadoAsync(int idEstablecimiento);
-        Task ActualizarMEC_POFDetalle(int idPOF, int supleAId, DateTime supleDesde, DateTime supleHasta);
+        Task ActualizarMEC_POFDetalle(int idPOF, int supleAId, int idCabecera, DateTime supleDesde, DateTime supleHasta);
         Task ConsolidarRegistrosAsync(int idCabecera, int idEstablecimiento, int usuario);
         Task CambiarEstadoCabeceraAsync(int idCabecera, int usuario);
     }
