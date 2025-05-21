42ff496287af778afc0ddfdf4105e115730df5f7
gcassulo <gcassulo@mardelplata.gob.ar>
2025-05-21 09:39:32 -0300

    correciones

--- CodigoFuente/antenas/API/Services/ImportacionMecanizada/ConsolidarMecanizadaService.cs
+++ CodigoFuente/antenas/API/Services/ImportacionMecanizada/ConsolidarMecanizadaService.cs
@@ -324,7 +324,7 @@ namespace API.Services
 
             // Paso 2: Consulta optimizada en un solo DbContext
             var query = _context.MEC_POF
-                .Where(pof => idsPOF.Contains(pof.IdPOF) && pof.TipoCargo == "S")
+                .Where(pof => idsPOF.Contains(pof.IdPOF) && pof.IdCarRevista == 2)
                 .Select(pof => new
                 {
                     Pof = pof,
