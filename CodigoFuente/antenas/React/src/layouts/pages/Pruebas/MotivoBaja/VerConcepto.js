3456ad2eaec1100703285f734888438d10c48ef1
leo <lramirez@mardelplata.gov.ar>
2025-05-29 12:04:31 -0300

    motb

---
+++ CodigoFuente/antenas/React/src/layouts/pages/Pruebas/MotivoBaja/VerConcepto.js
@@ -0,0 +1,98 @@
+// Importar componentes y librerías necesarias
+import Grid from "@mui/material/Grid";
+import MDBox from "components/MDBox";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MDButton from "components/MDButton";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Card from "@mui/material/Card";
+
+// Función principal del componente
+function VerConcepto() {
+  // Obtener el ID desde los parámetros de la URL
+  const { id } = useParams();
+  // Estado para almacenar los datos de la empresa
+  const [idConceptoData, setidConceptoData] = useState(null);
+  // Hook de navegación para redirigir a otras páginas
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+
+  // useEffect para realizar la petición GET al servidor cuando el ID cambie
+  useEffect(() => {
+    // Realizar la petición GET al servidor
+    axios
+      .get(process.env.REACT_APP_API_URL + `MotivoBaja/getbyid?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`, // Envía el token en los headers
+        },
+      })
+      .then((response) => {
+        // Guardar los datos recibidos en el estado
+        setidConceptoData(response.data);
+        console.log("Respuesta by ID:", idConceptoData);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos:", error);
+      });
+  }, [id]);
+
+  // Función para manejar la navegación a la página de crear nueva inspección (o cualquier otra funcionalidad)
+  const handleNuevoConcepto = () => {
+    navigate("/VerConceptoFE/Nuevo");
+  };
+
+  // Función para manejar la navegación a la página de editar
+  const handleEditarConcepto = (idConcepto) => {
+    const url = `/AltaConceptosFE/Edit/${idConcepto}`;
+    console.log(url);
+    navigate(url);
+  };
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      {idConceptoData && (
+        <>
+          <MDBox>
+            <Card>
+              <p className="tituloModal">{"Conceptos"}</p>
+              <div className="contenidoCard">
+                <p>
+                  <b>Con Aporte: </b>
+                  {idConceptoData?.conAporte === "S"
+                    ? "SI"
+                    : idConceptoData?.conAporte === "N"
+                    ? "NO"
+                    : "N/A"}
+                </p>
+                <p>
+                  <b>Patronal: </b>
+                  {idConceptoData?.patronal === "S"
+                    ? "SI"
+                    : idConceptoData?.patronal === "N"
+                    ? "NO"
+                    : "N/A"}
+                </p>
+                <p>
+                  <b>Vigente: </b>
+                  {idConceptoData?.vigente === "S"
+                    ? "SI"
+                    : idConceptoData?.vigente === "N"
+                    ? "NO"
+                    : "N/A"}
+                </p>
+                <MDButton onClick={() => handleEditarConcepto(idConceptoData?.idConcepto)}>
+                  Editar
+                </MDButton>
+              </div>
+            </Card>
+          </MDBox>
+        </>
+      )}
+    </DashboardLayout>
+  );
+}
+
+export default VerConcepto;
