ff8ca4b155ca776da874dfe8f4b6305c8de91760
gcassulo <gcassulo@mardelplata.gob.ar>
2025-06-05 10:30:41 -0300

    tipo movimientos y baja

---
+++ CodigoFuente/antenas/API/Controllers/MovimientosBajaController.cs
@@ -0,0 +1,67 @@
+﻿using API.DataSchema;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = "SuperAdmin, Admin")]
+    //[AllowAnonymous]
+    [Route("[controller]")]
+    public class MovimientosBajaController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ICRUDService<MEC_MovimientosBaja> _serviceGenerico;
+
+        public MovimientosBajaController(DataContext context, ILogger<MEC_MovimientosBaja> logger, ICRUDService<MEC_MovimientosBaja> serviceGenerico)
+        {
+            _context = context;
+            _serviceGenerico = serviceGenerico;
+        }
+        
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<IEnumerable<MEC_MovimientosBaja>>> Get() //TODO: el método no contiene await, ya que devuelve un IEnumerable, que no puede ser awaiteado, ver como se puede implementar
+        {
+            return Ok(_serviceGenerico.GetAll());
+        }
+
+        [HttpGet("GetByVigente")]
+        public async Task<ActionResult<IEnumerable<MEC_MovimientosBaja>>> GetByVigente([FromQuery] string vigente = null)
+        {
+            var result = await _serviceGenerico.GetByVigente(vigente);
+            return Ok(result);
+        }
+
+        [HttpGet("GetById")]
+        public async Task<ActionResult<MEC_MovimientosBaja>> Get(int Id)
+        {
+            return Ok(await _serviceGenerico.GetByID(Id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] MEC_MovimientosBaja baja)
+        {
+            await _serviceGenerico.Add(baja);
+            return Ok(baja);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            await _serviceGenerico.Delete(Id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<MEC_MovimientosBaja>> Update([FromBody] MEC_MovimientosBaja baja)
+        {
+            await _serviceGenerico.Update(baja);
+            return Ok(baja);
+        }
+
+    }
+}
