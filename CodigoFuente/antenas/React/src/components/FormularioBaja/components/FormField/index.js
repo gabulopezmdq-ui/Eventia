24c3309ac7ee2a22b072ce9150e6cc1bae43a168
efrontera <ernestorfrontera@gmail.com>
2025-06-10 09:19:14 -0300

    AltaRegistroBaja

---
+++ CodigoFuente/antenas/React/src/components/FormularioBaja/components/FormField/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MDInput from "components/MDInput";
+
+function FormField({
+  label,
+  name,
+  type,
+  formData,
+  handleChange,
+  readOnly = false,
+  disabled = false,
+}) {
+  return (
+    <MDInput
+      type={type}
+      label={label}
+      name={name}
+      value={formData[name] || ""}
+      onChange={!readOnly ? handleChange : undefined} // 👈 así evitás errores
+      variant="standard"
+      fullWidth
+      InputProps={{
+        readOnly: readOnly,
+      }}
+      disabled={disabled}
+    />
+  );
+}
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  formData: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  readOnly: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+
+export default FormField;
