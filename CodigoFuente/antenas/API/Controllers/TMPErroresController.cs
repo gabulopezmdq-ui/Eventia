9b9530b16242fa543c21f25b90141e5c85560891
gcassulo <gcassulo@mardelplata.gob.ar>
2025-06-10 09:22:43 -0300

    correciones

--- CodigoFuente/antenas/API/Controllers/TMPErroresController.cs
+++ CodigoFuente/antenas/API/Controllers/TMPErroresController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-nfronamespace API.Controllers
+namespace API.Controllers
 {
     [ApiController]
     //[Authorize(Roles = "SuperAdmin, Admin")]
