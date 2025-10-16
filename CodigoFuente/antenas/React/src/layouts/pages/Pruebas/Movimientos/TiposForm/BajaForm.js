24ff9e02a0ced9db3ed3cd4b0a271a8d794d1ea9
jcgarcia06 <gjuancruz65@gmail.com>
2025-10-16 11:57:29 -0300

    modifico modal ver y agrego modal de editar

---
+++ CodigoFuente/antenas/React/src/layouts/pages/Pruebas/Movimientos/TiposForm/BajaForm.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Grid, TextField } from "@mui/material";
+
+const BajaForm = ({ formValues, handleChange }) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <TextField
+          label="Apellido"
+          name="apellido"
+          value={formValues.apellido || ""}
+          fullWidth
+          InputProps={{ readOnly: true }}
+          disabled
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Nombre"
+          name="nombre"
+          value={formValues.nombre || ""}
+          fullWidth
+          InputProps={{ readOnly: true }}
+          disabled
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Observaciones"
+          name="observaciones"
+          value={formValues.observacionDetalle || ""}
+          fullWidth
+          InputProps={{ readOnly: true }}
+          multiline
+          rows={2}
+          disabled
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Motivo de Baja"
+          name="idMotivoBaja"
+          value={formValues.idMotivoBaja || ""}
+          fullWidth
+          InputProps={{ readOnly: true }}
+          disabled
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Observaciones Detalle"
+          name="observacionDetalle"
+          value={formValues.observaciones || ""}
+          onChange={handleChange}
+          fullWidth
+          multiline
+          rows={2}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Fecha Inicio de Baja"
+          name="fechaInicioBaja"
+          type="date"
+          value={formValues.fechaInicioBaja || ""}
+          onChange={handleChange}
+          fullWidth
+          InputLabelProps={{ shrink: true }}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Fecha Fin de Baja"
+          name="fechaFinBaja"
+          type="date"
+          value={formValues.fechaFinBaja || ""}
+          onChange={handleChange}
+          fullWidth
+          InputLabelProps={{ shrink: true }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+BajaForm.propTypes = {
+  formValues: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+export default BajaForm;
