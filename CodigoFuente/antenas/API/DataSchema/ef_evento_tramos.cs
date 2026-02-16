3268768a3bcd005fa35502f3a708c1a990820403
gabriela <gabu_lopez@hotmail.com>
2026-02-16 13:40:42 -0300

    null notas internas

--- CodigoFuente/antenas/API/DataSchema/ef_evento_tramos.cs
+++ CodigoFuente/antenas/API/DataSchema/ef_evento_tramos.cs
@@ -12,13 +12,13 @@ namespace API.DataSchema
 
         public string nombre { get; set; }
         public string leyenda_visible { get; set; }
-        public string notas_internas { get; set; }
+        public string? notas_internas { get; set; }
 
         public DateTimeOffset fecha_hora_inicio { get; set; }
         public DateTimeOffset? fecha_hora_fin { get; set; }
 
-        public string lugar { get; set; }
-        public string direccion { get; set; }
+        public string? lugar { get; set; }
+        public string? direccion { get; set; }
         public decimal? latitud { get; set; }
         public decimal? longitud { get; set; }
 
