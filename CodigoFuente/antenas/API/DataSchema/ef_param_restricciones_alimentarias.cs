f691dda8572f427fb46162c57ae4d26f5237bc6d
gabriela <gabu_lopez@hotmail.com>
2026-02-27 12:59:26 -0300

    correciones

--- CodigoFuente/antenas/API/DataSchema/ef_param_restricciones_alimentarias.cs
+++ CodigoFuente/antenas/API/DataSchema/ef_param_restricciones_alimentarias.cs
@@ -7,14 +7,21 @@ namespace API.DataSchema
     public class ef_param_restricciones_alimentarias
     {
         public long id_restriccion_alim { get; set; }
-        public string codigo { get; set; } = null!;
-        public string nombre { get; set; } = null!;
-        public int orden { get; set; }
-        public bool activo { get; set; }
 
-        public bool es_alergia { get; set; }
-        public short severidad { get; set; }
-        public bool requiere_alerta { get; set; }
-        public string? etiqueta_corta { get; set; }
+        public string codigo { get; set; } = null!;
+
+        public bool activo { get; set; } = true;
+
+        public int orden { get; set; } = 1;
+
+        public string categoria { get; set; } = "OTRA";
+
+        public string icon_key { get; set; } = "GENERIC";
+
+        public bool requiere_alerta_visual { get; set; } = true;
+
+        public bool requiere_confirmacion_organizador { get; set; } = false;
+
+        public bool es_alergeno { get; set; } = false;
     }
 }
