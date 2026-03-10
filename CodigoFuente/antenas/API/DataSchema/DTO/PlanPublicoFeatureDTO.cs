5ab40c3c54ae2eded5cd279b52219ef0fa643f54
gabriela <gabu_lopez@hotmail.com>
2026-03-10 15:02:34 -0300

    planes

---
+++ CodigoFuente/antenas/API/DataSchema/DTO/PlanPublicoFeatureDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DataSchema.DTO
+{
+    public class PlanPublicoFeatureDTO
+    {
+        public string codigo { get; set; } = null!;
+        public string nombre { get; set; } = null!;
+        public string categoria { get; set; } = null!;
+        public string? descripcion { get; set; }
+        public bool monetizable { get; set; }
+    }
+}
