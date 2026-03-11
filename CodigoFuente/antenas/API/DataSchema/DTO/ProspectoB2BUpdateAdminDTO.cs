f9ddbfc5de93be9a5e46c22e7bba31b7bb6d522f
gabriela <gabu_lopez@hotmail.com>
2026-03-11 09:57:32 -0300

    prospectos

---
+++ CodigoFuente/antenas/API/DataSchema/DTO/ProspectoB2BUpdateAdminDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace API.DataSchema.DTO
+{
+    public class ProspectoB2BUpdateAdminDTO
+    {
+        [JsonPropertyName("estado")]
+        public string? estado { get; set; }
+
+        [JsonPropertyName("nota_interna")]
+        public string? nota_interna { get; set; }
+
+        [JsonPropertyName("id_usuario_asignado")]
+        public long? id_usuario_asignado { get; set; }
+
+        [JsonPropertyName("proximo_contacto")]
+        public DateTimeOffset? proximo_contacto { get; set; }
+
+        [JsonPropertyName("activo")]
+        public bool? activo { get; set; }
+    }
+}
