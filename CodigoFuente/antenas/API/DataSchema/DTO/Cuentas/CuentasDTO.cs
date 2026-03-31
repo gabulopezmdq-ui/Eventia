99041f6e06f982a950ed12b734d43bb35f49866f
gabriela <gabu_lopez@hotmail.com>
2026-03-31 15:57:06 -0300

    cuentas

--- CodigoFuente/antenas/API/DataSchema/DTO/Cuentas/CuentasDTO.cs
+++ CodigoFuente/antenas/API/DataSchema/DTO/Cuentas/CuentasDTO.cs
@@ -9,6 +9,16 @@ namespace API.DataSchema.DTO.Cuentas
         public string tipo { get; set; }
         public string estado { get; set; }
         public long? id_plan { get; set; }
+
+        public string instagram { get; set; }
+        public string web { get; set; }
+        public string telefono { get; set; }
+        public string ciudad { get; set; }
+        public short? id_pais { get; set; }
+        public short? id_tipo_identificacion_fiscal { get; set; }
+        public string identificacion_fiscal { get; set; }
+        public string descripcion { get; set; }
+
         public DateTimeOffset fecha_alta { get; set; }
         public DateTimeOffset? fecha_modif { get; set; }
     }
@@ -17,5 +27,37 @@ namespace API.DataSchema.DTO.Cuentas
     {
         public string nombre_cuenta { get; set; }
         public string tipo { get; set; }
+
+        public string instagram { get; set; }
+        public string web { get; set; }
+        public string telefono { get; set; }
+        public string ciudad { get; set; }
+        public short? id_pais { get; set; }
+        public short? id_tipo_identificacion_fiscal { get; set; }
+        public string identificacion_fiscal { get; set; }
+        public string descripcion { get; set; }
+    }
+
+    public class cuenta_solicitar_request
+    {
+        public string nombre_cuenta { get; set; }
+        public string tipo { get; set; }
+
+        public string instagram { get; set; }
+        public string web { get; set; }
+        public string telefono { get; set; }
+        public string ciudad { get; set; }
+        public short? id_pais { get; set; }
+        public short? id_tipo_identificacion_fiscal { get; set; }
+        public string identificacion_fiscal { get; set; }
+        public string descripcion { get; set; }
+    }
+
+    public class cuenta_solicitar_response
+    {
+        public bool ok { get; set; }
+        public string mensaje { get; set; }
+        public long id_cuenta { get; set; }
+        public string estado { get; set; }
     }
 }
