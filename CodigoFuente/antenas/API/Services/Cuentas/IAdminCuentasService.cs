421aa30d9034afd56ff91c91c2cbd6fc77dd37cf
gabriela <gabu_lopez@hotmail.com>
2026-04-01 16:31:06 -0300

    cuentas

--- CodigoFuente/antenas/API/Services/Cuentas/IAdminCuentasService.cs
+++ CodigoFuente/antenas/API/Services/Cuentas/IAdminCuentasService.cs
@@ -10,5 +10,6 @@ namespace API.Services.Cuentas
         Task<admin_aprobar_cuenta_response> AprobarAsync(admin_aprobar_cuenta_request request, long id_usuario_admin);
         Task<admin_suspender_cuenta_response> SuspenderAsync(admin_suspender_cuenta_request request, long id_usuario_admin);
         Task<admin_cambiar_plan_response> CambiarPlanAsync(admin_cambiar_plan_request request, long id_usuario_admin);
+        Task<admin_reactivar_cuenta_response> ReactivarAsync(admin_reactivar_cuenta_request request, long id_usuario_admin);
     }
 }
