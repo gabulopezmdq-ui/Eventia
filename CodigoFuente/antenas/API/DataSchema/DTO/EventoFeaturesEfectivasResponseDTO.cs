22beea7a610b0436232742c7149e3b3175ed0a37
gabriela <gabu_lopez@hotmail.com>
2026-04-24 14:31:30 -0300

    features cuentas

--- CodigoFuente/antenas/API/DataSchema/DTO/EventoFeaturesEfectivasResponseDTO.cs
+++ CodigoFuente/antenas/API/DataSchema/DTO/EventoFeaturesEfectivasResponseDTO.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using API.DataSchema.DTO;
+using System.Collections.Generic;
 
-namespace API.DataSchema.DTO
+public class EventoFeaturesEfectivasResponseDTO
 {
-    public class EventoFeaturesEfectivasResponseDTO
-    {
-        public long id_evento { get; set; }
+    public long id_evento { get; set; }
 
-        public long? id_plan { get; set; }
-        public string? plan_codigo { get; set; }
-        public string? plan_nombre { get; set; }
+    public string scope_comercial { get; set; } = "EVENTO"; // EVENTO | CUENTA
 
-        public TrialDTO trial { get; set; } = new TrialDTO();
+    public long? id_cuenta { get; set; }
 
-        public List<AddonActivoDTO> addons_evento { get; set; } = new List<AddonActivoDTO>();
-        public List<FeatureEfectivaDTO> features { get; set; } = new List<FeatureEfectivaDTO>();
-    }
+    public long? id_plan { get; set; }
+    public string? plan_codigo { get; set; }
+    public string? plan_nombre { get; set; }
+
+    public TrialDTO? trial { get; set; }
+
+    public List<AddonActivoDTO> addons_evento { get; set; } = new List<AddonActivoDTO>();
+    public List<AddonActivoDTO> addons_cuenta { get; set; } = new List<AddonActivoDTO>();
+
+    public List<FeatureEfectivaDTO> features { get; set; } = new List<FeatureEfectivaDTO>();
 }
