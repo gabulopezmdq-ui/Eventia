73af1f33f69ef55fe444781a057148e757ee65cf
gabriela <gabu_lopez@hotmail.com>
2026-04-28 10:12:37 -0300

    autorizaciones personalizadas traducciones

---
+++ CodigoFuente/antenas/API/DataSchema/ef_programa_autorizacion_config_traducciones.cs
@@ -0,0 +1,26 @@
+﻿using API.DataSchema.Interfaz;
+using System;
+
+namespace API.DataSchema
+{
+    public class ef_programa_autorizacion_config_traducciones : IRegistroUnico
+    {
+        public long id_programa_autorizacion_config_traduccion { get; set; }
+
+        public long id_programa_autorizacion_config { get; set; }
+        public short id_idioma { get; set; }
+
+        public string titulo { get; set; } = null!;
+        public string? texto { get; set; }
+
+        public bool activo { get; set; }
+
+        public DateTimeOffset fecha_alta { get; set; }
+        public DateTimeOffset? fecha_modif { get; set; }
+
+        public string[] UniqueProperties => new[] { "id_programa_autorizacion_config", "id_idioma" };
+
+        public virtual ef_programa_autorizaciones_config? autorizacion_config { get; set; }
+        public virtual ef_idiomas? idioma { get; set; }
+    }
+}
