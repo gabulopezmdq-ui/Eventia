c58392db1baa7d3ad572a5d66cee73cf89b556e2
gabriela <gabu_lopez@hotmail.com>
2026-04-28 12:19:04 -0300

    landing inscripcion colonia

---
+++ CodigoFuente/antenas/API/DataSchema/DTO/Programas/ProgramaLandingIdiomaDTO.cs
@@ -0,0 +1,7 @@
+﻿public class ProgramaLandingIdiomaDTO
+{
+    public short IdIdioma { get; set; }
+    public string Locale { get; set; } = null!;
+    public string NombreLargo { get; set; } = null!;
+    public string? BanderaIso2 { get; set; }
+}
