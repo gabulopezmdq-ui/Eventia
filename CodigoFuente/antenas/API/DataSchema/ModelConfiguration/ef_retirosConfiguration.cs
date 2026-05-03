be6b85fb438671878bf61264c9687c6c8f189f84
gabriela <gabu_lopez@hotmail.com>
2026-05-03 20:05:00 -0300

    retiros qr

--- CodigoFuente/antenas/API/DataSchema/ModelConfiguration/ef_retirosConfiguration.cs
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/ef_retirosConfiguration.cs
@@ -5,86 +5,70 @@ namespace API.DataSchema.ModelConfiguration
 {
     public class ef_retirosConfiguration : IEntityTypeConfiguration<ef_retiros>
     {
-        public void Configure(EntityTypeBuilder<API.DataSchema.ef_retiros> builder)
+        public void Configure(EntityTypeBuilder<ef_retiros> builder)
         {
             builder.ToTable("ef_retiros", "public");
 
-            builder.HasKey(x => x.id_retiro)
-                .HasName("ef_retiros_pkey");
+            builder.HasKey(x => x.id_retiro);
 
-            builder.Property(x => x.id_retiro)
-                .HasColumnName("id_retiro")
-                .ValueGeneratedOnAdd();
+            builder.Property(x => x.id_retiro).ValueGeneratedOnAdd();
 
-            builder.Property(x => x.id_evento)
-                .HasColumnName("id_evento")
-                .IsRequired();
-
-            builder.Property(x => x.id_invitado_nino)
-                .HasColumnName("id_invitado_nino")
-                .IsRequired();
-
-            builder.Property(x => x.id_autorizacion)
-                .HasColumnName("id_autorizacion");
+            builder.Property(x => x.id_evento).IsRequired();
+            builder.Property(x => x.id_invitado_nino).IsRequired();
 
             builder.Property(x => x.nombre_retirador)
-                .HasColumnName("nombre_retirador")
                 .HasMaxLength(120)
                 .IsRequired();
 
             builder.Property(x => x.celular_retirador)
-                .HasColumnName("celular_retirador")
                 .HasMaxLength(40);
 
             builder.Property(x => x.metodo_validacion)
-                .HasColumnName("metodo_validacion")
-                .HasColumnType("char(1)")
+                .HasMaxLength(1)
                 .IsRequired();
 
             builder.Property(x => x.observaciones)
-                .HasColumnName("observaciones")
                 .HasMaxLength(200);
 
             builder.Property(x => x.fecha_retiro)
-                .HasColumnName("fecha_retiro")
-                .HasDefaultValueSql("now()")
+                .IsRequired()
+                .HasDefaultValueSql("now()");
+
+            builder.Property(x => x.fecha_operativa)
                 .IsRequired();
 
-            builder.Property(x => x.id_usuario_operador)
-                .HasColumnName("id_usuario_operador");
-
-            // Índices
             builder.HasIndex(x => new { x.id_evento, x.fecha_retiro })
                 .HasDatabaseName("ix_retiros_evento_fecha");
 
-            builder.HasIndex(x => x.id_invitado_nino)
+            builder.HasIndex(x => new { x.id_invitado_nino, x.fecha_retiro })
                 .HasDatabaseName("ix_retiros_nino_fecha");
 
-            // Foreign Keys
+            builder.HasIndex(x => new { x.id_evento, x.id_invitado_nino })
+                .HasDatabaseName("ix_retiros_evento_nino");
+
+            builder.HasIndex(x => new { x.id_evento, x.id_invitado_nino, x.fecha_operativa })
+                .IsUnique()
+                .HasDatabaseName("ux_retiros_unico_por_nino_dia");
+
             builder.HasOne(x => x.evento)
                 .WithMany()
                 .HasForeignKey(x => x.id_evento)
-                .HasConstraintName("fk_retiros_evento");
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder.HasOne(x => x.invitado_nino)
                 .WithMany()
                 .HasForeignKey(x => x.id_invitado_nino)
-                .HasConstraintName("fk_retiros_nino");
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder.HasOne(x => x.autorizacion)
                 .WithMany()
                 .HasForeignKey(x => x.id_autorizacion)
-                .HasConstraintName("fk_retiros_aut");
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder.HasOne(x => x.usuario_operador)
                 .WithMany()
                 .HasForeignKey(x => x.id_usuario_operador)
-                .HasConstraintName("fk_retiros_usuario");
-
-            // Check
-            builder.HasCheckConstraint(
-                "ck_retiros_metodo",
-                "metodo_validacion in ('A','M','O')");
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
