f44d5412f5b337594da3190ce7bc3145b718756c
gabriela <gabu_lopez@hotmail.com>
2026-05-04 21:59:39 -0300

    cocina-detalle

--- CodigoFuente/antenas/API/DataSchema/DTO/Programas/ProgramaCocinaDTO.cs
+++ CodigoFuente/antenas/API/DataSchema/DTO/Programas/ProgramaCocinaDTO.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace API.DataSchema.DTO.Programas
 {
     public class ProgramaCocinaDiaDTO
     {
         public long IdEvento { get; set; }
+        public string Programa { get; set; } = "";
         public DateOnly Fecha { get; set; }
         public string ServicioCodigo { get; set; } = "COMEDOR";
         public ProgramaCocinaResumenDTO Resumen { get; set; } = new();
@@ -54,5 +56,55 @@ namespace API.DataSchema.DTO.Programas
         public string Texto { get; set; } = "";
         public int Cantidad { get; set; }
         public bool AlertaVisual { get; set; }
+    }
+
+    public class ProgramaCocinaDetalleDTO
+    {
+        public long IdEvento { get; set; }
+        public string Programa { get; set; } = "";
+        public DateOnly Fecha { get; set; }
+        public string ServicioCodigo { get; set; } = "";
+
+        public ProgramaCocinaDetalleParticipanteDTO Participante { get; set; } = new();
+        public ProgramaCocinaDetalleResponsableDTO Responsable { get; set; } = new();
+
+        public List<ProgramaCocinaDetalleServicioDTO> ServiciosDelDia { get; set; } = new();
+        public List<ProgramaCocinaRestriccionDTO> Restricciones { get; set; } = new();
+
+        public ProgramaCocinaDetalleSaludDTO? Salud { get; set; }
+
+        public bool AlertaVisual { get; set; }
+        public string NivelAlerta { get; set; } = "NORMAL";
+    }
+
+    public class ProgramaCocinaDetalleParticipanteDTO
+    {
+        public long IdInvitado { get; set; }
+        public long IdRsvpGrupoIntegrante { get; set; }
+        public string NombreCompleto { get; set; } = "";
+    }
+
+    public class ProgramaCocinaDetalleResponsableDTO
+    {
+        public string NombreCompleto { get; set; } = "";
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+    }
+
+    public class ProgramaCocinaDetalleServicioDTO
+    {
+        public string Codigo { get; set; } = "";
+        public string Nombre { get; set; } = "";
+    }
+
+    public class ProgramaCocinaDetalleSaludDTO
+    {
+        public string? ObservacionesSalud { get; set; }
+        public bool TieneProblemaMedico { get; set; }
+        public string? ProblemaMedicoDetalle { get; set; }
+        public bool TieneAlergiasNoAlimentarias { get; set; }
+        public string? AlergiasNoAlimentariasDetalle { get; set; }
+        public string? NecesidadEspecial { get; set; }
+        public string? ObservacionesFamilia { get; set; }
     }
 }
