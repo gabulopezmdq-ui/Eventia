acacdd10eb3c4973d8f1693e25cbf0828181f82b
gabriela <gabu_lopez@hotmail.com>
2026-06-07 10:19:00 -0300

    features

---
+++ CodigoFuente/antenas/API/DataSchema/ModelConfiguration/ef_evento_feature_visibilidadConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using API.DataSchema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.ModelConfiguration
+{
+    public class ef_evento_feature_visibilidadConfiguration : IEntityTypeConfiguration<ef_evento_feature_visibilidad>
+    {
+        public void Configure(EntityTypeBuilder<ef_evento_feature_visibilidad> builder)
+        {
+            builder.ToTable("ef_evento_feature_visibilidad");
+
+            builder.HasKey(x => x.id_evento_feature_visibilidad);
+
+            builder.Property(x => x.id_evento_feature_visibilidad).HasColumnName("id_evento_feature_visibilidad");
+            builder.Property(x => x.id_evento).HasColumnName("id_evento").IsRequired();
+            builder.Property(x => x.id_feature).HasColumnName("id_feature").IsRequired();
+
+            builder.Property(x => x.visible_acceso_evento).HasColumnName("visible_acceso_evento");
+            builder.Property(x => x.visible_centro_evento).HasColumnName("visible_centro_evento");
+            builder.Property(x => x.visible_acceso_programa).HasColumnName("visible_acceso_programa");
+            builder.Property(x => x.visible_centro_programa).HasColumnName("visible_centro_programa");
+
+            builder.Property(x => x.fecha_alta).HasColumnName("fecha_alta").IsRequired();
+            builder.Property(x => x.fecha_modif).HasColumnName("fecha_modif");
+
+            builder.HasIndex(x => new { x.id_evento, x.id_feature })
+                .IsUnique()
+                .HasDatabaseName("ux_evento_feature_visibilidad");
+        }
+    }
+}
---
+++ CodigoFuente/antenas/API/DataSchema/ef_evento_feature_visibilidad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace API.DataSchema
+{
+    public class ef_evento_feature_visibilidad
+    {
+        public long id_evento_feature_visibilidad { get; set; }
+        public long id_evento { get; set; }
+        public long id_feature { get; set; }
+
+        public bool? visible_acceso_evento { get; set; }
+        public bool? visible_centro_evento { get; set; }
+        public bool? visible_acceso_programa { get; set; }
+        public bool? visible_centro_programa { get; set; }
+
+        public DateTime fecha_alta { get; set; }
+        public DateTime? fecha_modif { get; set; }
+    }
+}
