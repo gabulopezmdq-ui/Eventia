9ebe33e17404bbbe1d7e336fee2a38c09ab93e71
gabriela <gabu_lopez@hotmail.com>
2026-06-07 12:48:49 -0300

    features

---
+++ CodigoFuente/antenas/API/DataSchema/DTO/Eventos/Features/EventoFeatureVisibilidadBulkRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace API.DataSchema.DTO.Eventos.Features
+{
+    public class EventoFeatureVisibilidadBulkRequestDTO
+    {
+        public List<EventoFeatureVisibilidadItemDTO> items { get; set; } = new List<EventoFeatureVisibilidadItemDTO>();
+    }
+
+    public class EventoFeatureVisibilidadItemDTO
+    {
+        public long id_feature { get; set; }
+        public bool visible_acceso { get; set; }
+        public bool visible_centro { get; set; }
+    }
+}
